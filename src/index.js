3dddb11552688e4b3155eea6dd1ae307cd89f004
Hridoy Chowdhury <t.okay383@gmail.com>
2026-08-07 23:49:25 +0600

    mongodb server start function added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import { app } from "./app.js";
 import connectDB from "./db/connect.js";
+const port = process.env.PORT || 8000;
 
 
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`MongoDB Server is Runing On Port ${port}`);
+        })
+    } catch (err) {
+        console.log('Mongodb connection failed:', err);
+        process.exit(1);
+    }
+};
 
-connectDB()
+startServer();
